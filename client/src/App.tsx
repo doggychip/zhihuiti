5cf5e90261f328838a141eb43afccbb28614b31c
Ryan Cheung <rcheung@Ryans-MacBook-Pro-2.local>
2026-03-22 03:36:20 +0800

    Fix: use @/ alias for queryClient import in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route, Router } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
